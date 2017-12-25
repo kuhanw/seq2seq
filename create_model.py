f103c71c0756c549c55da589d41ac4df3306a452
unknown <kuhanw@gmail.com>
2017-12-25 18:27:42 +0800

    [Feature]: added custom decoder

--- create_model.py
+++ create_model.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import math
 from tensorflow.python.layers.core import Dense
 from tensorflow.python.util import nest
+from tensorflow.contrib.seq2seq.python.ops import beam_search_ops
 
 import data_formatting
 import pandas as pd
 import numpy as np
-
+import tf_helpers
 
 class Model:
     
@@ -44,9 +45,13 @@ class Model:
             self.initialize_placeholders()
             self.create_embeddings()
             self.create_encoder()
-            self.create_training_decoder()
-            self.create_training_module()            
+            if self.mode !='debug':
+                self.create_training_decoder()
+                self.create_training_module()            
         
+            if self.mode == 'debug':
+                self.create_inference_decoder()
+
         if self.mode == 'infer':
             
             self.encoder_output_keep = 1
@@ -179,28 +184,245 @@ class Model:
                                                                            beam_width=self.beam_width,
                                                                            output_layer=self.output_layer)
             
-            #self.finished, self.start_inputs, self.initial_state = self.inference_decoder.initialize()
-            
-            #self.outputs, self.next_state, self.next_inputs, self.finished = self.inference_decoder.step(0, self.start_inputs, self.initial_state)
-
-            
         if self.limit_decode_steps == True:
             max_decode_step = tf.reduce_max(self.encoder_inputs_length) + 5
         else:
             max_decode_step = None
-
-        (self.decoder_outputs_decode, self.decoder_last_state_decode,
+            
+        if self.mode!='debug':
+            
+            (self.decoder_outputs_decode, self.decoder_last_state_decode,
                  self.decoder_outputs_length_decode) = (tf.contrib.seq2seq.dynamic_decode(
                     decoder=self.inference_decoder,
                     output_time_major=False,
+                    #impute_finished=True,
                     maximum_iterations=max_decode_step))
            
-        if self.beam_width>1:
-            self.decoder_pred_decode = tf.identity(self.decoder_outputs_decode.predicted_ids, name='decoder_pred_decode')
-        else:
-            self.decoder_pred_decode = tf.argmax(self.decoder_outputs_decode.rnn_output, axis=-1, name='decoder_pred_decode')
+            if self.beam_width>1:
+                self.decoder_pred_decode = tf.identity(self.decoder_outputs_decode.predicted_ids, name='decoder_pred_decode')
+            else:
+                self.decoder_pred_decode = tf.argmax(self.decoder_outputs_decode.rnn_output, axis=-1, name='decoder_pred_decode')
 
+        elif self.mode=='debug':
+            
+            def _shape(batch_size, from_shape):
+                if not isinstance(from_shape, tf.TensorShape):
+                    return tensor_shape.TensorShape(None)
+                else:
+                    batch_size = tf.contrib.util.constant_value(tf.convert_to_tensor(batch_size, name="batch_size"))
+                    return tf.TensorShape([batch_size]).concatenate(from_shape)
 
+            def _create_ta(s, d):
+                return tf.TensorArray(
+                    dtype=d, size=0, dynamic_size=True, element_shape=_shape(self.batch_size, s))
+            
+            def condition(time, unused_outputs_ta, unused_state, unused_inputs, finished, unused_sequence_lengths):
+                return tf.logical_not(tf.reduce_all(finished))
+                #return tf.less(time, 50) 
+            
+            def step(time, outputs, state, inputs, finished, sequence_lengths):       
+                
+                prediction_lengths = state.lengths
+                previously_finished = state.finished
+                static_batch_size = tf.contrib.util.constant_value(self.batch_size)
+
+                cell_state = state.cell_state      
+
+                cell_inputs = nest.map_structure(lambda inp: self.inference_decoder._merge_batch_beams(inp, s=inp.shape[2:]), 
+                                      inputs)
+
+                cell_state = nest.map_structure(self.inference_decoder._maybe_merge_batch_beams, cell_state, 
+                                    self.inference_decoder._cell.state_size)
+                
+                with tf.variable_scope('decoder'):
+
+                    cell_outputs, next_cell_state = self.inference_decoder._cell(cell_inputs, cell_state)
+
+                cell_outputs = nest.map_structure(lambda out: self.inference_decoder._split_batch_beams(out, out.shape[1:]), 
+                                  cell_outputs)
+
+                next_cell_state = nest.map_structure(self.inference_decoder._maybe_split_batch_beams, next_cell_state, 
+                                     self.inference_decoder._cell.state_size)
+                with tf.variable_scope('decoder'):
+
+                    cell_outputs = self.inference_decoder._output_layer(cell_outputs)
+                
+              
+                logits = cell_outputs
+                step_log_probs = tf.nn.log_softmax(logits)
+                step_log_probs = tf_helpers._mask_probs(step_log_probs, end_token, previously_finished)
+
+                #Here is where we will add the anti-language model at a later date
+                total_probs = tf.expand_dims(state.log_probs, 2) + step_log_probs
+
+                vocab_size = logits.shape[-1].value or tf.shape(logits)[-1]
+
+                lengths_to_add = tf.one_hot(
+                  indices=tf.tile(tf.reshape(end_token, [1, 1]), [self.batch_size, self.beam_width]),
+                  depth=vocab_size,
+                  on_value=0,
+                  off_value=1)
+                
+                add_mask = (1 - tf.to_int32(previously_finished))
+
+                lengths_to_add = tf.expand_dims(add_mask, 2) * lengths_to_add
+
+                new_prediction_lengths = (lengths_to_add + tf.expand_dims(prediction_lengths, 2))
+                
+                scores = total_probs
+
+                scores_shape = tf.shape(scores)
+
+                time = tf.convert_to_tensor(time, name="time")
+
+                scores_flat = tf.cond(time > 0, lambda: tf.reshape(scores, [self.batch_size, -1]), lambda: scores[:, 0])
+
+                num_available_beam = tf.cond(time > 0, lambda: tf.reduce_prod(scores_shape[1:]), lambda: tf.reduce_prod(scores_shape[2:]))
+
+                # Pick the next beams according to the specified successors function
+                #At this state we have already calculated all the possible combinations of the previous step beam + current beam
+                #Thus we are evaluating the top N of sum of P(Current|Previous) + logP(Previous), thus any modification to the probs
+                #must be performed prior to this step
+                
+                next_beam_size = tf.minimum(tf.convert_to_tensor(self.beam_width, dtype=tf.int32, name="beam_width"),
+                                  num_available_beam)
+
+                next_beam_scores, word_indices = tf.nn.top_k(scores_flat, k=next_beam_size)
+                
+                next_beam_scores.set_shape([static_batch_size, self.beam_width])
+                
+                word_indices.set_shape([static_batch_size, self.beam_width])
+                
+                next_beam_probs = tf_helpers._tensor_gather_helper(
+                      gather_indices=word_indices,
+                      gather_from=scores,
+                      batch_size=self.batch_size,
+                      range_size=self.beam_width * vocab_size,
+                      gather_shape=[-1])
+
+                #Here, word indices represents the positions of the flattened 
+                #list of beams, which goes from 1 to beam_width*vocab_size
+                #thus the word indices obtained are not the word indices of 
+                #the 1 to N vocab but the flattened out list of logprobs from 1 to 
+                #beam_width*vocab_size
+                
+                next_word_ids = tf.to_int32(word_indices % vocab_size)
+                #The beam_ids represent which beams these word indices 
+                #(and thereby log prob values belong to), as
+                #we have word indices extracted out of 1 to beam_width*vocab_size
+                next_beam_ids = tf.to_int32(word_indices / vocab_size)               
+                
+                previously_finished = tf_helpers._tensor_gather_helper(
+                                              gather_indices=next_beam_ids,
+                                              gather_from=previously_finished,
+                                              batch_size=self.batch_size,
+                                              range_size=self.beam_width,
+                                              gather_shape=[-1])
+
+                next_finished = tf.logical_or(previously_finished, tf.equal(next_word_ids, end_token))
+                
+                lengths_to_add = tf.to_int32(tf.not_equal(next_word_ids, end_token))
+                lengths_to_add = (1 - tf.to_int32(next_finished)) * lengths_to_add
+                
+                next_prediction_len = tf_helpers._tensor_gather_helper(
+                                              gather_indices=next_beam_ids,
+                                              gather_from=state.lengths,
+                                              batch_size=self.batch_size,
+                                              range_size=self.beam_width,
+                                              gather_shape=[-1])
+        
+                next_prediction_len += lengths_to_add
+                
+                next_cell_state = nest.map_structure(
+                                  lambda gather_from: tf_helpers._maybe_tensor_gather_helper(
+                                  gather_indices=next_beam_ids,
+                                  gather_from=gather_from,
+                                  batch_size=self.batch_size,
+                                  range_size=self.beam_width,
+                                  gather_shape=[self.batch_size * self.beam_width, -1]), next_cell_state)
+               
+                #We have to transform back into beamdecoder class before passing back as the input and out must have same struct
+                #return beam_search_output, beam_search_state, next_inputs, finished
+            
+                beam_search_state = tf.contrib.seq2seq.BeamSearchDecoderState(
+                             cell_state=next_cell_state,
+                             log_probs=next_beam_probs,
+                             lengths=next_prediction_len,
+                             finished=next_finished)
+
+                beam_search_output = tf.contrib.seq2seq.BeamSearchDecoderOutput(
+                              scores=next_beam_scores,
+                              predicted_ids=next_word_ids,
+                              parent_ids=next_beam_ids)
+                
+                #Here we define the returns
+                finished = beam_search_state.finished
+                next_finished = finished
+                sample_ids = beam_search_output.predicted_ids
+                
+                #This part is most important, look up the new seq terms and take their embedding
+                #We pass that onto the decoder cell network in the next loop iteration
+
+                next_inputs = tf.cond(tf.reduce_all(finished), lambda: self.inference_decoder._start_inputs,
+                              lambda: self.inference_decoder._embedding_fn(sample_ids))
+                #return time, outputs, state, inputs, finished, sequence_lengths
+                
+                outputs = nest.map_structure(lambda ta, out: ta.write(time, out),
+                                      outputs, beam_search_output)
+               
+                next_sequence_lengths = tf.where(
+                  tf.logical_and(tf.logical_not(finished), next_finished),
+                      tf.fill(tf.shape(sequence_lengths), time + 1),
+                  sequence_lengths)
+                
+                return time+1, outputs, beam_search_state, next_inputs, finished, next_sequence_lengths
+                
+            #Initialize the decoder
+            self.time = 0
+            
+            self.finished, self.first_inputs, self.initial_state = self.inference_decoder.initialize()
+            initial_time = tf.constant(self.time)
+            #What is this for? initial_outputs_ta
+            initial_state = self.initial_state
+            initial_inputs = self.first_inputs
+            initial_finished = self.finished
+            initial_outputs_ta = nest.map_structure(_create_ta, self.inference_decoder.output_size,
+                                                    self.inference_decoder.output_dtype)
+            
+            initial_sequence_lengths = tf.zeros_like(initial_finished, dtype=tf.int32)
+            '''
+            #cell_state = initial_state.cell_state      
+
+            #cell_inputs = nest.map_structure(lambda inp: self.inference_decoder._merge_batch_beams(inp, s=inp.shape[2:]), 
+             #                                    initial_inputs)
+
+            #cell_state = nest.map_structure(self.inference_decoder._maybe_merge_batch_beams, cell_state, 
+             #                                   self.inference_decoder._cell.state_size)
+
+            #cell_outputs, self.next_cell_state = self.inference_decoder._cell(cell_inputs, cell_state)
+
+            #cell_outputs = self.inference_decoder._output_layer(cell_outputs)
+
+            #initial_logits = cell_outputs
+            '''
+            self.decode_loop = tf.while_loop(condition,
+                                        step,
+                                        loop_vars=[initial_time, initial_outputs_ta, initial_state, initial_inputs, 
+                                                   initial_finished, initial_sequence_lengths],
+                                        parallel_iterations=10,
+                                        swap_memory=False)
+            
+            final_outputs_ta = self.decode_loop[1]
+            self.final_state = self.decode_loop[2]
+            self.final_inputs = self.decode_loop[3]
+            
+            self.final_outputs = nest.map_structure(lambda ta: ta.stack(), final_outputs_ta)
+            self.final_sequence_lengths = self.decode_loop[5]
+            self.predicted_ids = beam_search_ops.gather_tree(
+                self.final_outputs.predicted_ids, self.final_outputs.parent_ids,
+                sequence_length=self.final_sequence_lengths)
+            #self.final_outputs, self.final_state = self.inference_decoder.finalize(self.final_outputs, 
+            #                                                                  self.final_state, self.final_sequence_lengths)
             
     def create_training_decoder(self):
 
@@ -239,14 +461,14 @@ class Model:
                                    time_major=False,
                                    name='training_helper')
         
-        training_decoder = tf.contrib.seq2seq.BasicDecoder(cell=self.decoder_cell,
+        self.training_decoder = tf.contrib.seq2seq.BasicDecoder(cell=self.decoder_cell,
                                                            helper=training_helper,
                                                            initial_state=self.decoder_initial_state, 
                                                            output_layer=self.output_layer)
         
         (self.decoder_outputs_train, self.decoder_last_state_train, self.decoder_outputs_length_train) = \
               (tf.contrib.seq2seq.dynamic_decode(
-                                                decoder=training_decoder,
+                                                decoder=self.training_decoder,
                                                 output_time_major=False,
                                                 impute_finished=True,
                                                 maximum_iterations=self.max_decoder_length
